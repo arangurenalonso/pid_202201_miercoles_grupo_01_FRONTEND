2de5745b23ddeea9bb802f258a1fb5dd25fa1efb
arangurenalonso <aranguren.alonso@gmail.com>
2022-04-20 20:53:30 -0500

    Mejora en el template de Admin

--- src/app/components/admin/visitante/pages/listado/listado.component.ts
+++ src/app/components/admin/visitante/pages/listado/listado.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Departamento } from 'src/app/model/departamento';
+import { Visitante } from 'src/app/model/visitante';
+import { DepartamentoService } from 'src/app/services/departamentoservice';
+import { VisitanteService } from 'src/app/services/visitante.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-listado',
@@ -6,10 +12,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listado.component.scss']
 })
 export class ListadoComponent implements OnInit {
-
-  constructor() { }
+  visitantes: Visitante[];
+  constructor(private visitanteService: VisitanteService,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.listarDepartamentos()
+
+  }
+  listarDepartamentos() {
+    this.activatedRoute.paramMap.subscribe(params => {
+      let page: number = +params.get('page');
+
+      if (!page) {
+        page = 0;
+      }
+      this.visitanteService.listarTodo()
+        .subscribe(response => {
+          console.log(response)
+          this.visitantes = response.detalle.data as Visitante[];
+          // this.paginador_padre = response;
+          // this.componente_name="departamentos"
+          
+        });
+
+    });
   }
 
+
+  delete(visitante: Visitante): void {
+    // Swal.fire({
+    //   title: `Está seguro de eliminar el departamento ${departamento.depnumero}?`,
+    //   showDenyButton: true,
+    //   showCancelButton: true,
+    //   confirmButtonText: 'Si',
+    //   denyButtonText: `No`,
+    // }).then((result) => {
+    //   /* Read more about isConfirmed, isDenied below */
+    //   if (result.isConfirmed) {
+    //     this.departamentoService.delete(departamento.id).subscribe(
+    //       () => {
+    //         Swal.fire('Departamento Eliminado con exito!', '', 'success')
+    //         this.listarDepartamentos()
+    //       }
+    //     )
+    //   } else if (result.isDenied) {
+    //     Swal.fire(`Departamento ${departamento.depnumero} no fue eliminado`, '', 'info')
+    //   }
+    // })
+  }
 }
+ 
